e31a5d2b142ddeea807416aa90bebd14e3dc40a5
Monika-M11 <04monika11@gmail.com>
2026-08-28 15:05:03 +0530

    changed some file

--- app/database.py
+++ app/database.py
@@ -6,9 +6,31 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv()
 
+if os.getenv("RENDER") is None:
+    load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 print("DATABASE_URL =", DATABASE_URL)
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
 
 engine = create_engine(DATABASE_URL)
 
